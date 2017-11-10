1434bb05a15eab04731b6620bdeb09ed74595fe2
zaid khan <zaidcool24@gmail.com>
2017-11-11 01:39:36 +0530

    Add files via upload

--- reddit_analysis/views.py
+++ reddit_analysis/views.py
@@ -16,6 +16,27 @@ import json
 import datetime
 import re
 from textblob import TextBlob
+import time
+
+
+
+
+def reddit_sentiment_analysis(reddit):
+	positive=0
+	negative=0
+	neutral=0
+	for element in reddit:
+		analysis=TextBlob(element['title'])
+		if analysis.sentiment.polarity>0:
+			positive+=1
+		if analysis.sentiment.polarity<0:
+			negative+=negative
+		else:
+			neutral+=1
+	polarity={'positive':positive,'negative':negative,'neutral':neutral}
+	return polarity
+
+
 
 def reddit_detailed_analysis(request):
 	if request.user.is_authenticated():
@@ -29,7 +50,19 @@ def reddit_detailed_analysis(request):
 			reddit=[]
 			it=0
 			temp_image='https://i.imgur.com/wesPbMx.jpg'
+			whenCreated = {}
+			dayofWeek = {}
 			for post in data['data']['children']:
+				d = time.strftime('%Y-%m-%d',time.localtime(post['data']['created_utc']))
+				if not d in whenCreated:
+					whenCreated[d] = 1
+				else:
+					whenCreated[d] += 1
+				weekDay = time.strftime('%A',time.localtime(post['data']['created_utc']))
+				if not weekDay in dayofWeek:
+					dayofWeek[weekDay] = 1
+				else:
+					dayofWeek[weekDay] += 1
 				reddit.append({'title':post['data']['title'],
 					           'permalink':post['data']['permalink'],
 					           'author':post['data']['author'],
@@ -40,6 +73,7 @@ def reddit_detailed_analysis(request):
 					           'thumbnail_height':post['data']['thumbnail_height'],
 					           'thumbnail_width':post['data']['thumbnail_width']
 					})
+			polarity=reddit_sentiment_analysis(reddit)
 			if 'https://www.reddit.com' in reddit[it]['url']:
 				reddit[it]['url']=""
 			if reddit[it]['thumbnail']=='self':
@@ -48,6 +82,9 @@ def reddit_detailed_analysis(request):
 			return render(request,'user/reddit.html',{'search_keyword':search_keyword,
 				                                       'search_form':form,
 				                                       'reddit':reddit,
+				                                       'whenCreated':whenCreated,
+				                                       'dayOfWeek':dayofWeek,
+				                                       'polarity':polarity
 				})
 		else:
 			if not flag:
